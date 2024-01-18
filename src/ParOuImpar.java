05c68519eb36383657948e8b3b744256edb41798
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-18 16:23:24 -0300

    Criando projeto

---
+++ src/ParOuImpar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite numero: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0)
+            System.out.println("o numero é par: " + numero);
+        else {
+            System.out.println("O numero é impar: " + numero);
+        }
+
+
+    }
+
+
+}
---
+++ src/Tabuada.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
