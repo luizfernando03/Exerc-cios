0ea21583ca7d89270cfdcfa4b06d242c97d16a68
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-18 16:55:34 -0300

    subindo logica da class tabuada

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -4,8 +4,16 @@ public class Tabuada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Digite um numero para obter o resultado desejado na tabuada : ");
+        int numero = scanner.nextInt();
 
+        System.out.println("Tabuada do numero" + numero + " : ");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        scanner.close();
+        
     }
 }
 
