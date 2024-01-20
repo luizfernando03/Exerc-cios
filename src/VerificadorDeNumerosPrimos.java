46897e11941cc3eec9d2d3548460a1d9b0a3b8ba
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-19 23:06:37 -0300

    Subindo logica da class VerificadorDenumeroPrimo

--- src/VerificadorDeNumerosPrimos.java
+++ src/VerificadorDeNumerosPrimos.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class VerificadorDeNumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero inteiro : ");
+        int numero = scanner.nextInt();
+
+        if (numero <= 1) {
+            System.out.println("O numero" + numero + " não é um numero primo");
+
+
+        }
+        for (int i = 2; i <= numero ; i++) {
+            if (numero % i == 0 && i == numero) {
+                System.out.println("O numero" + numero + " é um numero primo");
+                break;
+            } else if (numero % i == 0) {
+                System.out.println("o numero" + numero + " não é primo ");
+            }
+        }
+        
+    }
+
 }
