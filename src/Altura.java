21e102cc3a2af6c6cd4d07a144812bc29afff055
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-23 15:57:40 -0300

    Subindo dados da class Altura

--- src/Altura.java
+++ src/Altura.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Altura {
 
-    
+    public static void main(String[] agrs){
+        Scanner scanner = new Scanner (System.in);
+
+        double maiorAltura =0 ;
+        double manorAltura = 999;
+        double somaAlturaHomens = 0;
+        int umeroMulheres =0;
+
+        //10 pessoas
+        for(int i=0 ; i<10 ; i++ ){
+            System.out.println("informe a altura");
+
+        }
+    }
 
 }
