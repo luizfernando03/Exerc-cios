f069c07cefd9b85aba46d802dd0020fd8134242e
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-22 21:33:51 -0300

    Criando Class SaldoClientes

---
+++ src/SaldoCliente.java
@@ -0,0 +1,26 @@
+public class SaldoCliente {
+    public static void main(String[] args) {
+
+        double [] cliente1 = {1500 , -2500, 300, 100};
+        double [] cliente2 = {2000 , 1000, -1200, -800};
+        double [] cliente3 = {1000, 4500, 2300 , 900};
+
+        double saldoFinalClinte1 = calcularSaldoFinal (cliente1);
+        double saldoFinalClinte2 = calcularSaldoFinal (cliente2);
+        double saldoFinalClinte3 = calcularSaldoFinal (cliente3);
+
+        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte1);
+        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte2);
+        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte3);
+
+    }
+
+    private static double calcularSaldoFinal(double[] cliente) {
+        return 0;
+
+        for  (double valor )
+
+    }
+
+
+}
