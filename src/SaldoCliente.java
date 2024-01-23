da425ff7a88446eb0d326eaf17b9cd335273fdf2
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-22 22:18:09 -0300

    Subindo logica da Class SaldoClientes

--- src/SaldoCliente.java
+++ src/SaldoCliente.java
@@ -1,22 +1,28 @@
 public class SaldoCliente {
     public static void main(String[] args) {
 
-        int [] cliente1 = {1500 , -2500, 300, 100};
-        int [] cliente2 = {2000 , 1000, -1200, -800};
-        int [] cliente3 = {1000, 4500, 2300 , 900};
+        int[] cliente1 = {1500, -2500, 3000, 100};
+        int[] cliente2 = {2000, 1000, -1200, -800};
+        int[] cliente3 = {1000, 4500, 2300, 900};
 
-        int saldoFinalClinte1 = calcularSaldoFinal (cliente1);
-        int saldoFinalClinte2 = calcularSaldoFinal (cliente2);
-        int saldoFinalClinte3 = calcularSaldoFinal (cliente3);
+        int saldoFinalClinte1 = calcularSaldoFinal(cliente1);
+        int saldoFinalClinte2 = calcularSaldoFinal(cliente2);
+        int saldoFinalClinte3 = calcularSaldoFinal(cliente3);
 
-        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte1);
-        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte2);
-        System.out.println("Saldo final do cliente1: "  + saldoFinalClinte3);
+        System.out.println("Saldo final do cliente1: " + saldoFinalClinte1);
+        System.out.println("Saldo final do cliente1: " + saldoFinalClinte2);
+        System.out.println("Saldo final do cliente1: " + saldoFinalClinte3);
 
     }
 
-    private static int calcularSaldoFinal(int[] cliente) {
-        return 0;
+    private static int calcularSaldoFinal(int[] saldoInicial) {
+        int saldoFinal = 0;
+
+        for (int valor : saldoInicial) {
+            saldoFinal += valor;
+
+        }
+        return saldoFinal;
 
     }
 
