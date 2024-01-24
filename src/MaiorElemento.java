3c7632380896676c11747afeb7584b9b2f083be8
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-24 13:51:27 -0300

    Atualizando

--- src/MaiorElemento.java
+++ src/MaiorElemento.java
@@ -1,5 +1,13 @@
 public class MaiorElemento {
+    public static void main(String[] args){
+        int [][] matriz = {{1,2,4}, {5,8,9}, {23,24,46},{14,21,11}};
 
-    
+        int maiorElemento = encontrarMaiorElemento(matriz);
+    }
+    private static int encontrarMaiorElemento(int[][] matriz) {
+        int maiorElemento =  matriz [0][0];
+        return maiorElemento;
+    }
+
 
 }
