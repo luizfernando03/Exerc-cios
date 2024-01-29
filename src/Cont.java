9722bd0cf11f20d819b5a6377f07597895d54ae2
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-29 11:56:19 -0300

    Acrescentando do while

--- src/Cont.java
+++ src/Cont.java
@@ -7,11 +7,18 @@ public class Cont {
         System.out.println("contando ate " + max);
 
 
-        while (i <= max ){
+        while (i <= max) {
             System.out.println("valor de i : " + i);
             i++;
 
-            System.out.println("Valor de i; " + i);
+            System.out.println("Valor de i; " + i); //Valor de 11
+
+            do {
+                i++;
+                System.out.println("valor de i : " + i);
+            } while (i < 15);
+
+            System.out.println(i);
         }
 
     }
