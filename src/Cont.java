b2883afb5a0dea468513268b05bc4b8c59fef1eb
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-01-29 11:42:46 -0300

    logica com while

--- src/Cont.java
+++ src/Cont.java
@@ -1,4 +1,17 @@
 public class Cont {
+    public static void main(String[] args) {
 
-    
+        int i = 1;
+        int max = 10;
+
+        System.out.println("contando ate " + max);
+
+
+        while (i < max ){
+            System.out.println("valor de i : " + i);
+            i++;
+        }
+
+    }
+
 }
